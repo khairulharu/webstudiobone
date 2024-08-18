272a4ef9e5f0b54cb20bd8131ab488661c4cfec0
khairulharu <khairulaswad719@gmail.com>
2024-08-18 14:27:45 +0800

    add header content

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,18 @@
 <nav>
-    <div>
-        <p>WebStudioBone</p>
+    <div class="py-5 bg-gray-900 text-center flex justify-center text-white gap-1">
+        <p class="font-bold text-4xl">@</p>
+        <p class="font-bold text-4xl">w</p>
+        <p class="font-bold text-4xl">e</p>
+        <p class="font-bold text-4xl">b</p>
+        <p class="font-bold text-4xl">s</p>
+        <p class="font-bold text-4xl">t</p>
+        <p class="font-bold text-4xl">u</p>
+        <p class="font-bold text-4xl">d</p>
+        <p class="font-bold text-4xl">i</p>
+        <p class="font-bold text-4xl">o</p>
+        <p class="font-bold text-4xl">b</p>
+        <p class="font-bold text-4xl">o</p>
+        <p class="font-bold text-4xl">n</p>
+        <p class="font-bold text-4xl">e</p>
     </div>
 </nav>
