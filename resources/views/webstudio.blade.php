43ec1620e4cb35ce768f4c5aa9bf4843ddd39d87
khairulharu <khairulaswad719@gmail.com>
2024-08-18 13:53:07 +0800

    add tailwind

--- resources/views/webstudio.blade.php
+++ resources/views/webstudio.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>webstudio</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <x-navbar></x-navbar>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
 </body>
 </html>
