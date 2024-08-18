ae3ac3beabc14fa3d7b19bd16b2ede003e6e2128
khairulharu <khairulaswad719@gmail.com>
2024-08-18 14:51:13 +0800

    change style

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,18 +1,5 @@
 <nav>
     <div class="py-5 bg-gray-900 text-center flex justify-center text-white gap-1">
-        <p class="font-bold text-4xl">@</p>
-        <p class="font-bold text-4xl">w</p>
-        <p class="font-bold text-4xl">e</p>
-        <p class="font-bold text-4xl">b</p>
-        <p class="font-bold text-4xl">s</p>
-        <p class="font-bold text-4xl">t</p>
-        <p class="font-bold text-4xl">u</p>
-        <p class="font-bold text-4xl">d</p>
-        <p class="font-bold text-4xl">i</p>
-        <p class="font-bold text-4xl">o</p>
-        <p class="font-bold text-4xl">b</p>
-        <p class="font-bold text-4xl">o</p>
-        <p class="font-bold text-4xl">n</p>
-        <p class="font-bold text-4xl">e</p>
+        <p class="font-bold sm:text-2xl lg:text-4xl text-xl">@webstudiobone</p>
     </div>
 </nav>
