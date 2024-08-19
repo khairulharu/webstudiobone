2f688566c5b47c6e0de3d04c9a6e2135766cb250
khairulharu <khairulaswad719@gmail.com>
2024-08-19 10:27:02 +0800

    perbaiki footer dan kontak

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,28 @@
 {{-- tambahkan footerr untuk kontak wa dan instagram dan juga copyrigth website dan berikan gambar ungu seperti di component header --}}
+<div class="relative isolate overflow-hidden px-6 pt-16 shadow-2xl sm:px-16 md:pt-24 lg:px-24 lg:pt-0">
+     <svg viewBox="0 0 1024 1024"
+         class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+         aria-hidden="true">
+         <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+             fill-opacity="0.7" />
+         <defs>
+             <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                 <stop stop-color="#7775D6" />
+                 <stop offset="1" stop-color="#E935C1" />
+             </radialGradient>
+         </defs>
+     </svg>
+     <div class="py-5 text-4xl tracking-tight font-extrabold text-center text-white">
+          <p>Kontak Kami</p>
+     </div>
+     <div class="text-gray-400">
+          <ul>
+               <a href="" class="hover:underline hover:text-primary-500"><li>Instagram : @webstudiobone</li></a>
+               <a href="" class="hover:underline hover:text-primary-500"><li>Email : khairulaswad719@gmail.com</li></a>
+               <a href="" class="hover:underline hover:text-primary-500"><li>WhatsApp: +628985277576</li></a>
+          </ul>
+     </div>
+     <div class="text-white py-3 flex justify-center">
+          <p>copyright webstudiobone@gmail.com</p>
+     </div>
+</div>
