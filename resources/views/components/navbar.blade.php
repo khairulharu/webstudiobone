775d194d073d73f106adfe3f81af86f4ce8b3421
khairulharu <khairulaswad719@gmail.com>
2024-08-22 19:11:00 +0800

    cha

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav>
     <div class="py-8 text-center flex justify-center text-white gap-1">
-        <p class="font-bold sm:text-3xl lg:text-4xl text-2xl tracking-widest">WEBSTUDIOBONE</p>
+        <p class="font-bold sm:text-3xl lg:text-4xl text-4xl tracking-widest">WEBSTUDIOBONE</p>
     </div>
 </nav>
